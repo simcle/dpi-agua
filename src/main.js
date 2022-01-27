1db6c8781d85c6fc1bd13ffc72f369c3cfe5411a
nendi <nendisofinady84@gmail.com>
2022-01-27 11:48:24 +0700

    new repository

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import './index.css'
+import './assets/icons/icomoon/styles.min.css'
+
 Vue.config.productionTip = false
 
 new Vue({
